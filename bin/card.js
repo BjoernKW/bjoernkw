0244530a0ca73fe07e4b4c9f5506fee78a416859
Björn Wilmsmann <bjoern@wilmsmann.de>
2018-12-21 16:55:40 +0100

    Added my personal details

--- bin/card.js
+++ bin/card.js
@@ -14,33 +14,29 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
-  labelWork: chalk.white.bold('      Work:'),
+  name: chalk.white('Björn Wilmsmann /'),
+  handle: chalk.cyan('BjoernKW'),
+  work: chalk.white('Independent IT Consultant'),
+  web: chalk.cyan('https://bjoernkw.com'),
+  twitter: chalk.cyan('https://twitter.com/bwilmsmann'),
+  github: chalk.cyan('https://github.com/BjoernKW'),
+  npx: chalk.white('npx bjoernkw'),
+  labelWeb: chalk.white.bold('       Web:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
-  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
-  labelWeb: chalk.white.bold('       Web:'),
   labelCard: chalk.white.bold('      Card:')
 }
 
 // Actual strings we're going to output
 const newline = '\n'
 const heading = `${data.name} ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
+const working = `${data.work}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
+const output = heading + newline + newline + working + newline + newline + webing + newline + twittering + newline + githubing + newline + newline + carding
 
 console.log(chalk.green(boxen(output, options)))
